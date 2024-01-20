b9cc1e091c9507ce66481eb1d7078b2374265608
SudeepMR <136821853+Sudeep-MR@users.noreply.github.com>
2024-01-20 10:14:04 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,20 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'music/song1.m4a',
+        artist: 'artist1',
+        cover: 'images/blue eyes.jpg'
+    },
+    {
+        name: 'song 2',
+        path: 'music/song2.mp3',
+        artist: 'artist1',
+        cover: 'images/desi kalakr.jpeg'
+    },
+    {
+        name: 'song 3',
+        path: 'music/song3.mp3',
+        artist: 'artist1',
+        cover: 'images/Brown-Rang-DJ-VICKY-NYC.jpeg'
+    }
+]
